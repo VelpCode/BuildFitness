09d473ef5dc96cf440ee05272a61b6eb3143c5d7
Aman Velpula <velpmail@gmail.com>
2023-11-13 15:56:33 -0500

    heroSection functionality

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { Box } from '@mui/material';
 
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+
 const Home = () => {
   return (
     <Box>
